ca318c1c0bce31260ba53cb93f057c2f123a494f
Ruben Villalon <rubenvillalond@gmail.com>
2020-05-25 21:25:13 +0200

    landingPage Added

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,31 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom'
 import Authentication from './components/Authentication'
 import WithAuth from './HOC/withAuth'
 import SideBar from './components/SideBar'
 import UserProfile from './components/UserProfile'
 import Page404 from './components/Tools/404'
 import Home from './components/Home'
+import LandingPage from './components/LandingPage'
 
 
-export const ROUTE_AUTHENTICATION = '/'
+export const LANDING_PAGE = '/'
+export const LOGIN = '/login'
 export const ROUTE_HOME = '/home'
 export const ROUTE_USERPROFILE = '/userprofile'
 
 const Routes = () => {
     return <>
         <Router>
-            <SideBar>
-                <Switch>
-                    <Route exact path={ROUTE_AUTHENTICATION} component={Authentication}/>
+            <Switch>
+                <Route exact path={LANDING_PAGE} component={LandingPage}/>
+                <SideBar>
+                    <Route exact path={LOGIN} component={Authentication}/>
                     <Route exact path={ROUTE_HOME} component={WithAuth(Home)}/>
                     <Route exact path={ROUTE_USERPROFILE} component={WithAuth(UserProfile)}/>
-                    <Route component={Page404}/>
-                </Switch>
-            </SideBar>
+                </SideBar>
+                <Route component={Page404}/>
+            </Switch>
         </Router>
     </>
 }
