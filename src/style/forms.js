e92bca07bec9c2eed375b0d4e0763f89a8a3b61f
Ruben Villalon <rubenvillalond@gmail.com>
2020-05-27 15:23:58 +0200

    refactor forms styles

--- src/style/forms.js
+++ src/style/forms.js
@@ -1,25 +1,22 @@
 import styled from 'styled-components/macro'
 
+
 export const FormBase = styled.form`
     width: 400px;
     height: 400px;
     background: rgba(255,255,255,0.66);
-    border-radius: 10px;
+    border-radius: ${props => props.theme.borderRadius};
     display: flex;
     flex-direction: column;
     justify-content: space-around;
     align-items: center;
-    box-shadow: -6px 8px 21px -4px rgba(142,142,142,0.73);
-    position: relative;
-    color: black;
+    box-shadow: ${props => props.theme.boxShadow};
 `
 
 export const LoginForm = styled(FormBase)``
 export const RegistrationForm = styled(FormBase)``
 export const ResetPasswordForm = styled(FormBase)``
-export const PasswordResetValidationForm = styled(FormBase)`
-    
-`
+export const PasswordResetValidationForm = styled(FormBase)``
 export const RegistrationValidationForm = styled(FormBase)`
   height: 600px;
 `
