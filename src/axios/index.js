c3e967221bf16f157c5f852ab24e4b0e1565ab96
roncaglionidaniele <roncaglionidaniele@icloud.com>
2020-07-17 17:00:09 +0200

    variable baseurl

--- src/axios/index.js
+++ src/axios/index.js
@@ -7,7 +7,10 @@ let baseUrl = ''
 if(process.env.NODE_ENV === 'development'){
     baseUrl = 'http://localhost:8000/backend/api/'
 } else {
-    baseUrl = 'https://templates.propulsion-home.ch/backend/api/'
+    // Works only for client side SPA apps
+    if(window.location.host.includes('propulsion-home.ch')){
+        baseUrl = `${window.location.origin}/backend/api/`
+    }
 }
 
 const Axios = axios.create({
