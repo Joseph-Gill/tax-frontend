02026a1db6027a5cbfe4174b91800603a2295e12
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-12-01 12:45:13 +0100

    style error message text

--- src/style/messages.js
+++ src/style/messages.js
@@ -2,5 +2,8 @@ import styled from 'styled-components/macro'
 
 
 export const ErrorMessage = styled.span`
-    color: ${props => props.theme.colorFail}
+    color: ${props => props.theme.colorFail};
+    font-weight: 600;
+    font-family: ${props => props.theme.nunitoFontFamily};
+    font-size: 10px;
 `
