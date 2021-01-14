9f4f27542a70d79229cd5a190d7cc2f15271a205
Joseph Edwin Gill <josephedwingill@gmail.com>
2021-01-14 15:02:00 +0100

    setup fade in on member table

--- src/style/tables.js
+++ src/style/tables.js
@@ -6,6 +6,43 @@ export const CommentTable = styled.table`
     border: 1px solid ${props => props.theme.grayFour};
     border-collapse: collapse;
     border-radius: ${props => props.theme.borderRadius};
+    animation: fadein 0.5s;
+    -moz-animation: fadein 0.5s; /* Firefox */
+    -webkit-animation: fadein 0.5s; /* Safari and Chrome */
+    -o-animation: fadein 0.5s; /* Opera */
+
+    @keyframes fadein {
+        from {
+            opacity:0;
+        }
+        to {
+            opacity:1;
+        }
+    }
+    @-moz-keyframes fadein { /* Firefox */
+        from {
+            opacity:0;
+        }
+        to {
+            opacity:1;
+        }
+    }
+    @-webkit-keyframes fadein { /* Safari and Chrome */
+        from {
+            opacity:0;
+        }
+        to {
+            opacity:1;
+        }
+    }
+    @-o-keyframes fadein { /* Opera */
+        from {
+            opacity:0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
 `
 
 export const TableTitleRow = styled.tr`
