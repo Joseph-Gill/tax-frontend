d6bc6897abe816a7d48c0b19256593c3ea7b95ed
Joseph Edwin Gill <josephedwingill@gmail.com>
2021-01-25 17:59:45 +0100

    Fix styling on AddEntityModal country selector

--- src/components/Modals/AddEntityModal/index.jsx
+++ src/components/Modals/AddEntityModal/index.jsx
@@ -61,20 +61,8 @@ const AddEntityModal = ({cancelNewEntityLinkHandler, countryName, error, legalFo
                 <div>
                     <ActiveInputLabel>Location</ActiveInputLabel>
                     <CountryDropdown
+                        classes='profileCountryDropdown'
                         onChange={(val) => setCountryName(val)}
-                         // eslint-disable-next-line react/forbid-component-props
-                        style={{
-                            width: '302px',
-                            height: '42px',
-                            fontSize: '14px',
-                            fontWeight: '600',
-                            lineHeight: '19px',
-                            background: '#FAFAFA',
-                            border: '1px solid #D3D8DD',
-                            borderRadius: '4px',
-                            fontFamily: 'Nunito Sans, sans-serif',
-                            paddingLeft: '7px',
-                        }}
                         value={countryName}
                     />
                     <EntityErrorContainer>
