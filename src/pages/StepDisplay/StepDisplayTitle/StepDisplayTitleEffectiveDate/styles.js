89984a5c3957d972e596b2d4b1e3199db94e0bd4
Joseph Edwin Gill <josephedwingill@gmail.com>
2021-01-27 16:13:04 +0100

    Fix padding on disabled input Effective Date of DisplayStep

--- src/pages/StepDisplay/StepDisplayTitle/StepDisplayTitleEffectiveDate/styles.js
+++ src/pages/StepDisplay/StepDisplayTitle/StepDisplayTitleEffectiveDate/styles.js
@@ -6,7 +6,6 @@ import {AddNewStepButton} from '../../../../style/buttons'
 export const DisabledDateInput = styled(BaseInput)`
     width: 128px;
     height: 32px;
-    text-align: center;
 `
 
 export const DateInputAddStepButtonContainer = styled.div`
