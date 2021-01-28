d1d145225fd2b64bdd3e5ec22337676461c35bbf
Joseph Edwin Gill <josephedwingill@gmail.com>
2021-01-28 10:47:04 +0100

    Fix styling on Groups page

--- src/pages/Groups/styles.js
+++ src/pages/Groups/styles.js
@@ -5,7 +5,7 @@ import {AuthenticatedPageSectionTitle} from '../../style/titles'
 export const GroupDisplayContainer = styled.div`
     Width: 860px;
     background: ${props => props.theme.white};
-    padding: 10px 20px 16px 20px;
+    padding: 10px 20px 10px 20px;
     margin: 20px 0;
     box-shadow: ${props => props.theme.boxShadow};
     border-radius: ${props => props.theme.borderRadius};
