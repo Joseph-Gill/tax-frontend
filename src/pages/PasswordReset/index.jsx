022482046d4f36c163fb0b1cb951b43680e8970b
Joseph Edwin Gill <josephedwingill@gmail.com>
2021-03-04 12:15:30 +0000

    update Password Reset to use Formik

--- src/pages/PasswordReset/index.jsx
+++ src/pages/PasswordReset/index.jsx
@@ -1,10 +1,12 @@
-import React, {useRef, useState} from 'react'
+import React, {useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
+import {Formik} from 'formik'
 import SignUpLink from '../../components/SignUpLink'
 import SuccessMessage from '../../components/SuccessMessage'
 import LoginFooter from '../../components/LoginFooter'
 import PasswordLink from '../../components/PasswordLink'
-import {useResetErrors} from '../../hooks'
+import LoginLogo from '../../components/LoginLogo'
+import {resetErrors} from '../../store/errors/actions/errorAction'
 import {resetPassword} from '../../store/user/actions/authentication/resetPasswordAction'
 import {LOGIN} from '../../routes/paths'
 import {Title} from '../../style/titles'
@@ -14,51 +16,75 @@ import {ErrorMessage} from '../../style/messages'
 import {ResetPasswordForm} from '../../style/forms'
 import {ActiveInputLabel} from '../../style/labels'
 import {BasePageContainer, ErrorMessageContainer} from '../../style/containers'
-import LoginLogo from '../../components/LoginLogo'
 
 
 const PasswordReset = () => {
     const dispatch = useDispatch()
-    let email = useRef('')
     const error = useSelector(state => state.errorReducer.error)
     const [showSuccess, setShowSuccess] = useState(false)
 
-    useResetErrors()
-
-    const resetPasswordHandler = async e => {
-        e.preventDefault()
-        const data = await dispatch(resetPassword(email.current.value))
-        if(data) setShowSuccess(!showSuccess)
-    }
-
     return (
         <BasePageContainer>
-            <ResetPasswordForm>
-                <LoginLogo />
-                {showSuccess &&
-                    <SuccessMessage
-                        message="A code has been sent to your email!"
-                        redirect={LOGIN}
-                    />}
-                <Title>Forgot Your Password?</Title>
-                <div>
-                    <ActiveInputLabel>Email</ActiveInputLabel>
-                    <BaseInput
-                        name='email'
-                        placeholder='Enter your email'
-                        ref={email}
-                        type='text'
-                    />
-                </div>
-                <ErrorMessageContainer>
-                    {error && <ErrorMessage>{error.email}</ErrorMessage>}
-                    {error && <ErrorMessage>{error.detail}</ErrorMessage>}
-                </ErrorMessageContainer>
-                <BaseButton onClick={resetPasswordHandler}>Send Code</BaseButton>
-                <PasswordLink />
-                <SignUpLink />
-                <LoginFooter />
-            </ResetPasswordForm>
+            {showSuccess &&
+                <SuccessMessage
+                    message="A code has been sent to your email!"
+                    redirect={LOGIN}
+                />}
+            <Formik
+                // Sets initial values for Formik inputs
+                initialValues={{email: ''}}
+                // Function called each time submit button is clicked
+                onSubmit={async (values) => {
+                    dispatch(resetErrors())
+                    const response = await dispatch(resetPassword(values.email))
+                    if (response) {
+                        setShowSuccess(!showSuccess)
+                    }
+                }}
+                validate={values => {
+                    const errors = {};
+                    if (!values.email) {
+                        errors.email = true;
+                    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                        errors.email = 'Invalid email address';
+                    }
+                    return errors
+                }}
+                // Validation only checked when user clicks submit
+                validateOnBlur={false}
+                validateOnChange={false}
+            >
+                {({values, errors, touched, handleChange,
+                      handleBlur, handleSubmit, isSubmitting}) => (
+                          <ResetPasswordForm onSubmit={handleSubmit}>
+                              <LoginLogo />
+                              <Title>Forgot Your Password?</Title>
+                              <div>
+                                  <ActiveInputLabel>Email</ActiveInputLabel>
+                                  <BaseInput
+                                      error={errors.email}
+                                      name='email'
+                                      onBlur={handleBlur}
+                                      onChange={handleChange}
+                                      placeholder='Enter your email'
+                                      type='email'
+                                      value={values.email}
+                                  />
+                              </div>
+                              <ErrorMessageContainer>
+                                  {errors.email && touched.email && <ErrorMessage>{errors.email}</ErrorMessage>}
+                                  {error && <ErrorMessage>{error.detail}</ErrorMessage>}
+                                  {error.email && <ErrorMessage>{error.email}</ErrorMessage>}
+                              </ErrorMessageContainer>
+                              <BaseButton disabled={isSubmitting} type="submit">
+                                  Send Code
+                              </BaseButton>
+                              <PasswordLink />
+                              <SignUpLink />
+                              <LoginFooter />
+                          </ResetPasswordForm>
+                )}
+            </Formik>
         </BasePageContainer>
     )
 }
