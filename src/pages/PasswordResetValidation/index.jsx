0478815b7fb2cf18e2065abe9c404164b70b90fa
Joseph Edwin Gill <josephedwingill@gmail.com>
2021-03-04 13:47:22 +0000

    update Password Reset Validation to use Formik

--- src/pages/PasswordResetValidation/index.jsx
+++ src/pages/PasswordResetValidation/index.jsx
@@ -1,18 +1,20 @@
-import React, { useRef, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
+import React, {useState } from "react"
+import {useDispatch, useSelector} from "react-redux"
+import {Formik} from 'formik'
 import SignUpLink from "../../components/SignUpLink"
 import SuccessMessage from "../../components/SuccessMessage"
 import PasswordLink from '../../components/PasswordLink'
 import LoginFooter from '../../components/LoginFooter'
 import LoginLogo from '../../components/LoginLogo'
-import { useUrlQueryParams, useResetErrors } from "../../hooks"
-import { restPasswordValidate } from "../../store/user/actions/authentication/resetPasswordAction"
+import {useUrlQueryParams} from "../../hooks"
+import {resetErrors} from '../../store/errors/actions/errorAction'
+import {restPasswordValidate} from "../../store/user/actions/authentication/resetPasswordAction"
 import {LOGIN} from '../../routes/paths'
-import { Title } from "../../style/titles"
-import { BaseInput } from "../../style/inputs"
-import { BaseButton } from "../../style/buttons"
+import {Title} from "../../style/titles"
+import {BaseInput} from "../../style/inputs"
+import {BaseButton} from "../../style/buttons"
 import {ActiveInputLabel} from '../../style/labels'
-import { ErrorMessage } from "../../style/messages"
+import {ErrorMessage} from "../../style/messages"
 import {PasswordResetValidationForm} from '../../style/forms'
 import {BasePageContainer, ErrorMessageContainer} from '../../style/containers'
 
@@ -21,67 +23,98 @@ const PasswordResetValidation = () => {
     const dispatch = useDispatch()
     const email = useUrlQueryParams("email")
     const code = useUrlQueryParams("code")
-    let password = useRef("")
-    let password_repeat = useRef("")
     const error = useSelector(state => state.errorReducer.error)
     const [showSuccess, setShowSuccess] = useState(false)
 
-    useResetErrors()
-
-    const register = async e => {
-        e.preventDefault()
-        const credentials = {
-            email: email,
-            code: code,
-            password: password.current.value,
-            password_repeat: password_repeat.current.value,
-        }
-        const data = await dispatch(restPasswordValidate(credentials))
-        if (data) setShowSuccess(!showSuccess)
-    }
-
     return (
         <BasePageContainer>
-            <PasswordResetValidationForm>
-                <LoginLogo />
-                {showSuccess && (
-                    <SuccessMessage
-                        message="Your Password has been updated successfully!"
-                        redirect={LOGIN}
-                    />
+            {showSuccess && (
+                <SuccessMessage
+                    message="Your Password has been updated successfully!"
+                    redirect={LOGIN}
+                />
+            )}
+            <Formik
+                // Sets initial values for Formik inputs
+                initialValues={{password: '', password_repeat: ''}}
+                // Function called each time submit button is clicked
+                onSubmit={async (values) => {
+                    dispatch(resetErrors())
+                    const credentials = {
+                        email: email,
+                        code: code,
+                        password: values.password,
+                        password_repeat: values.password_repeat
+                    }
+                    const response = await dispatch(restPasswordValidate(credentials))
+                    if (response) {
+                        setShowSuccess(!showSuccess)
+                    }
+                }}
+                validate={values => {
+                    const errors = {};
+                    if (!values.password) {
+                        errors.password = true;
+                    } else if (!values.password_repeat) {
+                        errors.password_repeat = true;
+                    } else if (values.password !== values.password_repeat) {
+                        errors.password = 'Password and Password Repeat do not match'
+                        errors.password_repeat = true;
+                    }
+                    return errors
+                }}
+                // Validation only checked when user clicks submit
+                validateOnBlur={false}
+                validateOnChange={false}
+            >
+                {({values, errors, touched, handleChange,
+                      handleBlur, handleSubmit, isSubmitting}) => (
+                          <PasswordResetValidationForm onSubmit={handleSubmit}>
+                              <LoginLogo />
+                              <Title>Create New Password</Title>
+                              <div>
+                                  <ActiveInputLabel>Password</ActiveInputLabel>
+                                  <BaseInput
+                                      error={errors.password}
+                                      name='password'
+                                      onBlur={handleBlur}
+                                      onChange={handleChange}
+                                      placeholder='Set a new password'
+                                      type='password'
+                                      value={values.password}
+                                  />
+                              </div>
+                              <ErrorMessageContainer>
+                                  {error && <ErrorMessage>{error.password}</ErrorMessage>}
+                                  {errors.password && touched.password && <ErrorMessage>{errors.password}</ErrorMessage>}
+                              </ErrorMessageContainer>
+                              <div>
+                                  <ActiveInputLabel>Password</ActiveInputLabel>
+                                  <BaseInput
+                                      error={errors.password_repeat}
+                                      name='password_repeat'
+                                      onBlur={handleBlur}
+                                      onChange={handleChange}
+                                      placeholder='Retype new password'
+                                      type='password'
+                                      value={values.password_repeat}
+                                  />
+                              </div>
+                              <ErrorMessageContainer>
+                                  {error && <ErrorMessage>{error.password_repeat}</ErrorMessage>}
+                                  {error && <ErrorMessage>{error.non_field_errors}</ErrorMessage>}
+                                  {error && <ErrorMessage>{error.detail}</ErrorMessage>}
+                                  {errors.password_repeat && touched.password_repeat && <ErrorMessage>{errors.password_repeat}</ErrorMessage>}
+                              </ErrorMessageContainer>
+                              <BaseButton disabled={isSubmitting} type="submit">
+                                  Reset Password
+                              </BaseButton>
+                              <PasswordLink />
+                              <SignUpLink />
+                              <LoginFooter />
+                          </PasswordResetValidationForm>
                 )}
-                <Title>Create New Password</Title>
-                <div>
-                    <ActiveInputLabel>Password</ActiveInputLabel>
-                    <BaseInput
-                        name='password'
-                        placeholder='Set a new password'
-                        ref={password}
-                        type='password'
-                    />
-                </div>
-                <ErrorMessageContainer>
-                    {error && <ErrorMessage>{error.password}</ErrorMessage>}
-                </ErrorMessageContainer>
-                <div>
-                    <ActiveInputLabel>Password</ActiveInputLabel>
-                    <BaseInput
-                        name='password_repeat'
-                        placeholder='Retype new password'
-                        ref={password_repeat}
-                        type='password'
-                    />
-                </div>
-                <ErrorMessageContainer>
-                    {error && <ErrorMessage>{error.password_repeat}</ErrorMessage>}
-                    {error && <ErrorMessage>{error.non_field_errors}</ErrorMessage>}
-                    {error && <ErrorMessage>{error.detail}</ErrorMessage>}
-                </ErrorMessageContainer>
-                <BaseButton onClick={register}>Reset Password</BaseButton>
-                <PasswordLink />
-                <SignUpLink />
-                <LoginFooter />
-            </PasswordResetValidationForm>
+            </Formik>
         </BasePageContainer>
     )
 }
