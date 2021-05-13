56c763903fe3d26e5d935ca9f2b322e3688f3f03
Joseph Edwin Gill <josephedwingill@gmail.com>
2021-05-13 13:28:37 +0100

    Fix logos on ChooseGroupModal cards

--- src/components/Modals/ChooseGroupModal/ModalGroupCard/styles.js
+++ src/components/Modals/ChooseGroupModal/ModalGroupCard/styles.js
@@ -6,10 +6,11 @@ export const ModalGroupCardContainer = styled.div`
     height: 84.84px;
     display: flex;
     align-items: center;
-    justify-content: flex-start;
+    justify-content: flex-end;
     border: 1px solid ${props => props.theme.grayFour};
     border-radius: ${props => props.theme.borderRadius};
     padding: 15px;
+    position: relative;
 
     :hover {
         cursor: pointer;
@@ -20,15 +21,19 @@ export const ModalGroupCardContainer = styled.div`
 `
 
 export const ModalGroupCardImage = styled.img`
-    width: 85px;
-    height: 54px;
-    margin-right: 15px;
+    position: absolute;
+    max-width: 85px;
+    height: auto;
+    max-height: 72px;
+    left: 15px;
 `
 
 export const ModalGroupCardText = styled.h2`
     font-family: ${props => props.theme.spartanFontFamily};
     font-weight: 600;
     font-size: 16px;
+    max-width: 150px;
+    text-align: right;
     line-height: 18px;
     color: ${props => props.theme.grayOne};
 `
